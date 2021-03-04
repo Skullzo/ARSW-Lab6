0bd5fb6ac1bc791813f703620fe23b85d6036cdc
Alejandro Toro Daza <skullzo@outlook.com>
2021-03-04 16:21:15 -0500

    Código Fuente

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -17,4 +17,46 @@ app= (function (){
             document.getElementById("puntosLabel").innerHTML = valorTotal;
         }
     };
+
+    var _funcDraw = function (vari) {
+        if (vari) {
+            var lastx = null;
+            var lasty = null;
+            var actx = null;
+            var acty = null;
+            var c = document.getElementById("myCanvas");
+            var ctx = c.getContext("2d");
+
+            ctx.clearRect(0, 0, 500, 500);
+            ctx.beginPath();
+
+            vari.points.map(function (prue){
+                if (lastx == null) {
+                    lastx = prue.x;
+                    lasty = prue.y;
+                } else {
+                    actx = prue.x;
+                    acty = prue.y;
+                    ctx.moveTo(lastx, lasty);
+                    ctx.lineTo(actx, acty);
+                    ctx.stroke();
+                    lastx = actx;
+                    lasty = acty;
+                }
+            });
+        }
+    }
+    return {
+            plansAuthor: function () {
+                author = document.getElementById("autor").value;
+                apimok.getBlueprintsByAuthor(author,_funcModify);
+
+            },
+
+            drawPlan: function(name) {
+                author = document.getElementById("autor").value;
+                obra = name;
+                apimok.getBlueprintsByNameAndAuthor(author,obra,_funcDraw);
+            }
+        };
 })();
