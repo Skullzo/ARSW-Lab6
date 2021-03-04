37f88b3424f58644e5646d37bd35ae49d567f4ae
Alejandro Toro Daza <skullzo@outlook.com>
2021-03-04 11:48:33 -0500

    Código Fuente

--- src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -1,5 +1,5 @@
+/*
 package edu.eci.arsw.blueprints.test.services;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class ApplicationServicesTests {
@@ -16,5 +15,5 @@ public class ApplicationServicesTests {
     @Test
     public void contextLoads() { 
     }
-
 }
+*/
