ecbbb2911279984e2dcbc291b9fdc6984432077c
Alejandro Toro Daza <skullzo@outlook.com>
2021-03-04 14:50:47 -0500

    Código Fuente

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,20 @@
+app= (function (){
+    var _funcModify = function (variable) {
+        if(variable != null){
+            var arreglo = variable.map(function(blueprint){
+                return {key:blueprint.name, value:blueprint.points.length}
+            })
+            $("#tabla tbody").empty();
+            arreglo.map(function(blueprint){
+                var temporal = '<tr><td id="nombreActor">'+blueprint.key+'</td><td id="puntos">'+blueprint.value+'</td><td type="button" onclick="app.drawPlan(\''+blueprint.key+'\')">Open</td></tr>';
+                $("#tabla tbody").append(temporal);
+            })
+
+            var valorTotal = arreglo.reduce(function(total, valor){
+                return total.value + valor.value;
+            })
+            document.getElementById("autorLabel").innerHTML = author;
+            document.getElementById("puntosLabel").innerHTML = valorTotal;
+        }
+    };
+})();
